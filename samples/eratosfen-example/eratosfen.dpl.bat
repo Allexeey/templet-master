ba5a2b1b186e35541e97ee10fbea2a138ed593aa
Allexeey <113130948+Allexeey@users.noreply.github.com>
2023-05-06 15:03:17 +0400

    Delete samples/eratosfen-example directory

--- samples/eratosfen-example/eratosfen.dpl.bat
+++
@@ -1,2 +0,0 @@
-"../../bin/gen" -deploy eratosfen.cpp eratosfen.dpl.cpp
-"../../bin/skel" -i eratosfen.cpp -s eratosfen.dpl.cpp
--- samples/eratosfen-example/eratosfen.dsg.bat
+++
@@ -1,2 +0,0 @@
-"../../bin/gen" -design eratosfen.cpp eratosfen.dsg.cpp
-"../../bin/skel" -i eratosfen.cpp -s eratosfen.dsg.cpp
